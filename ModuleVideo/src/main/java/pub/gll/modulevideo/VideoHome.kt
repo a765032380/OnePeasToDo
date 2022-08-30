a82040b362c3f059ba8b1cd89178af258149b176
贡磊磊 <>
2022-08-30 19:10:24 +0800

    --

--- ModuleVideo/src/main/java/pub/gll/modulevideo/VideoHome.kt
+++ ModuleVideo/src/main/java/pub/gll/modulevideo/VideoHome.kt
@@ -1,42 +1,35 @@
 package pub.gll.modulevideo
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
-import coil.ImageLoader
-import coil.compose.AsyncImage
-import coil.compose.rememberAsyncImagePainter
 import coil.decode.VideoFrameDecoder
-import coil.request.ImageRequest
 import coil.request.Parameters
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import pub.gll.modulevideo.model.VideoItemModel
+import pub.gll.modulevideo.video.icons.PlayArrow
 import pub.gll.modulevideo.vm.VideoVM
 import pub.gll.onepeas.libbase.Test
 import pub.gll.onepeas.libbase.arouter.Launch
-import kotlin.random.Random
 
 @Composable
 fun VideoHome(lazyListState:LazyListState) {
@@ -127,19 +120,27 @@ fun VideoItem(data: VideoItemModel?) {
             val parameters = Parameters.Builder()
                 .set(VideoFrameDecoder.VIDEO_FRAME_MICROS_KEY, videoFrameMicros)
                 .build()
+            Icon(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(5.dp))
+                    .background(Color.Gray.copy(alpha = 0.5f))
+                    .height(300.dp),
+                imageVector = Icons.Rounded.PlayArrow,
+                contentDescription ="" )
 
-            AsyncImage(modifier = Modifier
-                .fillMaxWidth()
-                .clip(RoundedCornerShape(5.dp))
-                .height(300.dp),
-                contentScale = ContentScale.FillWidth,
-                model = ImageRequest.Builder(LocalContext.current)
-                        .apply(block = fun ImageRequest.Builder.() {
-                            data(data = data?.icon)
-                            parameters(parameters = parameters)
-                        })
-                        .build()
-                , contentDescription = null)
+//            AsyncImage(modifier = Modifier
+//                .fillMaxWidth()
+//                .clip(RoundedCornerShape(5.dp))
+//                .height(300.dp),
+//                contentScale = ContentScale.FillWidth,
+//                model = ImageRequest.Builder(LocalContext.current)
+//                        .apply(block = fun ImageRequest.Builder.() {
+//                            data(data = data?.icon)
+//                            parameters(parameters = parameters)
+//                        })
+//                        .build()
+//                , contentDescription = null)
             Text(
                 text = "${data?.name}",
             )
--- ModuleVideo/src/main/java/pub/gll/modulevideo/VideoListAct.kt
+++ ModuleVideo/src/main/java/pub/gll/modulevideo/VideoListAct.kt
@@ -7,12 +7,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -23,6 +21,9 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import pub.gll.onepeas.libbase.activity.BaseActivity
 import pub.gll.onepeas.libbase.arouter.ARouterPath
+import pub.gll.onepeas.libbase.widget.AppSnackBar
+import pub.gll.onepeas.libbase.widget.SNACK_ERROR
+import pub.gll.onepeas.libbase.widget.popupSnackBar
 
 @AndroidEntryPoint
 @Route(path = ARouterPath.VIDEO_LIST)
@@ -30,12 +31,12 @@ class VideoListAct: BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
             val lazyListState = rememberLazyListState()
             val coroutineScope = rememberCoroutineScope()
+            val scaffoldState = rememberScaffoldState()
             var editMessageShown by remember { mutableStateOf(false) }
-
             suspend fun showEditMessage() {
+                popupSnackBar(coroutineScope, scaffoldState, label = SNACK_ERROR, "搜索")
                 if (!editMessageShown) {
                     editMessageShown = true
                     delay(3000L)
@@ -56,6 +57,13 @@ class VideoListAct: BaseActivity() {
                 content = {
                     VideoHome(lazyListState = lazyListState)
 //                    PreviewVideoItem()
+                },
+                snackbarHost = {
+                    SnackbarHost(
+                        hostState = scaffoldState.snackbarHostState
+                    ) { data ->
+                        AppSnackBar(data = data)
+                    }
                 }
             )
         }
@@ -95,7 +103,7 @@ private fun HomeFloatingActionButton(
             modifier = Modifier.padding(horizontal = 16.dp)
         ) {
             Icon(
-                imageVector = Icons.Default.Edit,
+                imageVector = Icons.Default.Search,
                 contentDescription = null
             )
             // Toggle the visibility of the content with animation.
